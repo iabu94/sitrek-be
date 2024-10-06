920858de44409cf565bf12259dff73d208f5fbc1
Abdullah Mansoor <abdullah.mansoor@osos.om>
2024-10-06 17:19:36 +0530

    changes

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -10,9 +10,7 @@ export class RolesService {
 
   async findAll(): Promise<Role[]> {
     const result = await this.dataSource.query(
-      `SELECT 
-      r.id as id,
-      r.name as name
+      `SELECT r.id as id, r.name as name
       FROM ${tableName} as r`,
     );
     return result;
